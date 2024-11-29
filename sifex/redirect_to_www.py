32b8cf353c53442fb0991e8e58acc73361cb675d
leoproducts <masanja.developer@gmail.com>
2024-11-29 22:38:26 +0300

    Update redirect_to_www

--- sifex/redirect_to_www.py
+++ sifex/redirect_to_www.py
@@ -1,18 +1,12 @@
 from django.http import HttpResponsePermanentRedirect
 
 class RedirectToWWWMiddleware:
-    """
-    Middleware to redirect non-www requests (e.g., sifex.co.tz) to www (e.g., www.sifex.co.tz).
-    """
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         host = request.get_host()
-
-        # Redirect only if the hostname is sifex.co.tz
+        # Redirect only if the hostname is exactly sifex.co.tz
         if host == "sifex.co.tz":
             return HttpResponsePermanentRedirect(f"https://www.sifex.co.tz{request.get_full_path()}")
-
-        # Otherwise, continue processing the request
         return self.get_response(request)

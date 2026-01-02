c9b99e16ec18b32e415168b3a74c3d6a8945ab59
leoproducts <masanja.developer@gmail.com>
2026-01-02 08:48:37 +0300

    update settings staatic storage

--- sifex/settings.py
+++ sifex/settings.py
@@ -173,7 +173,8 @@ STATICFILES_DIRS = [
 ]
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 
 
 # Media files (Uploaded by users)
